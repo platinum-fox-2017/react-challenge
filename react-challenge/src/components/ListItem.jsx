005e42ffafdc29c562da38af7dd0bc08c91576b2
Chandra Budiman <cbudiman1000@gmail.com>
2018-04-03 12:59:27 +0700

    Release 0 - initial setup

---
+++ react-challenge/src/components/ListItem.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ListItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        
+      </div>
+    )
+  }
+}
