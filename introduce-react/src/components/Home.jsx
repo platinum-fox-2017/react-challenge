0c9355fcb157f196a8dffe399dbeb6dd84f1c0db
Devdots <devdots@Devdotss-MBP.localdomain>
2018-04-03 20:08:38 +0700

    add wallpaper

--- introduce-react/src/components/Home.jsx
+++ introduce-react/src/components/Home.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 class Home extends React.Component {
   render () {
     return (
-      <h1>Welcome To My Dota2 Team Guides</h1>
+      <div className="container">
+        <h1>Welcome To My Dota2 Team Guides</h1>
+        <img src="http://orig12.deviantart.net/d676/f/2015/199/2/d/the_international_2015_dota_2_teams_by_jaredftw-d91tm7h.png" alt="team" width="900" height="400" />
+      </div>
     )
   }
 }
