0100886fb0e52243d5ba5e25e3de23bb8d02efe7
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-04-04 13:43:12 +0700

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import ArticleList from './components/ArticleList'
 import ArticleDetail from './components/ArticleDetail'
+import NotFound from './components/NotFound'
 
 class App extends Component {
 
@@ -11,8 +12,11 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <h1>HackerNews</h1>
-          <Route exact path="/" component={ArticleList} />
-          <Route exact path="/story/:id" component={ArticleDetail} />
+          <Switch>
+            <Route exact path="/" component={ArticleList} />
+            <Route exact path="/story/:id" component={ArticleDetail} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
       </div>
     );
