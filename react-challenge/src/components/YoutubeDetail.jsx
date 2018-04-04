0e6cbb10edd8f50112dcae9f9234a12b50f4092d
Chandra Budiman <cbudiman1000@gmail.com>
2018-04-04 13:46:48 +0700

    fixed video url

--- react-challenge/src/components/YoutubeDetail.jsx
+++ react-challenge/src/components/YoutubeDetail.jsx
@@ -30,17 +30,16 @@ export default class YoutubeDetail extends Component {
         <div className="row">
         {
           this.state.videoData.map((data, i) => {
-            console.log("yt detail, ",data);
+            // console.log("yt detail, ",data);
+            // console.log('==== ',this.props.match.params.id)
             var frame = <div className="col-xs-12" key={i}><h3>{data.snippet.title}</h3><p>Channel : {data.snippet.channelTitle}</p>
-            <iframe width="560" height="320" src={ytLink+data.id.videoId} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            <iframe width="560" height="320" src={ytLink+this.props.match.params.id} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
             <p>{data.snippet.description}</p>
             </div>
             return frame
           })
         }
-
         {this.frame}
-        {/* <iframe width="560" height="315" src={ytLink+item.id.videoId} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>           */}
         </div>
       </div>
     )
