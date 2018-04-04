06be1a2176bd4180d9379c456b7069aec76c48fe
unknown <fatimah.azzhr@gmail.com>
2018-04-04 14:00:04 +0700

    before trying redux

--- swapi/src/App.js
+++ swapi/src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src="https://pokemontcg.io/assets/images/pokemon-minimalist-30bc8a16a6a63e980a4e06c11a66638b.png" className="App-logo" alt="logo" />
-            {/* <h1 className="App-title">All the Star Wars data you've ever wanted!</h1> */}
           </header>
           <Switch>
             <Route exact path="/" component={ PeopleList } />
--- swapi/src/components/Character.jsx
+++ swapi/src/components/Character.jsx
@@ -4,22 +4,30 @@ class Character extends Component {
   render() {
     return (
       <div>
-        <h1>{ this.props.cardData.name }</h1>
-        <img src={ this.props.cardData.imageUrlHiRes } className="singlecard" alt="singlecard"/>
-        <p>HP: { this.props.cardData.hp }</p>
-        <p>Type: { this.props.cardData.types }</p>
-        <p>Rarity: { this.props.cardData.rarity }</p>
-        {/* <p>Weakness: { this.props.cardData.weaknesses }</p> */}
-        <p>Attacks:</p>
-          {/* <ul>
-            {
-              this.props.cardData.attacks.map((attack, i) => {
-                return (
-                  <li>{ `${attack.name}, damage: ${attack.damage}` }</li>
-                )
-              })
-            }
-          </ul> */}
+        <div>
+          <h1>{ this.props.cardData.name }</h1>
+        </div>
+        <div className="pokes">
+          <div className="poke">
+            <img src={ this.props.cardData.imageUrlHiRes } className="singlecard" alt="singlecard"/>
+          </div>
+          <div className="poke">
+            <p>HP: { this.props.cardData.hp }</p>
+            <p>Type: { this.props.cardData.types }</p>
+            <p>Rarity: { this.props.cardData.rarity }</p>
+            {/* <p>Weakness: { this.props.cardData.weaknesses }</p> */}
+            <p>Attacks:</p>
+              {/* <ul>
+                {
+                  this.props.cardData.attacks.map((attack, i) => {
+                    return (
+                      <li>{ `${attack.name}, damage: ${attack.damage}` }</li>
+                    )
+                  })
+                }
+              </ul> */}
+          </div>
+        </div>
       </div>
     );
   }
