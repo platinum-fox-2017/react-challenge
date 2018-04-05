d5aed36585228d303fcbf234e605fea238bf8d60
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-04-05 11:17:42 +0700

    Add ringloader on fetch commentlist

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { RingLoader } from 'react-spinners'
 
 const CommentList = (props) => {
   const {comments} = props
-  return comments.map(comment => {
-    return (
-      <div className="table">
-          <div className="table-row" key={ comment.id}>
-            <span style={{ width: '50%'}}>
-              {  comment.text}
-            </span>
-            <span style={{ width: '20%'}}>{ comment.by}</span>
-          </div>
-      </div>
-    )
-  })
+  if (comments.length) {
+    return comments.map(comment => {
+      return (
+        <div className="table">
+            <div className="table-row" key={ comment.id}>
+              <span style={{ width: '50%'}}>
+                {  comment.text}
+              </span>
+              <span style={{ width: '20%'}}>{ comment.by}</span>
+            </div>
+        </div>
+      )
+    })
+  } else {
+    return (<div className="centered"> <RingLoader /></div>)
+  }
 }
 
 const mapStateToProps = (state) => {
